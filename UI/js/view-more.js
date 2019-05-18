26ba010dec7cf8d3d77ba611fe55a39264c3f33d
iambright <captainamiedi1@gmail.com>
2019-05-18 20:24:19 +0100

    thumbnail added

--- UI/js/view-more.js
+++ UI/js/view-more.js
@@ -18,3 +18,15 @@ const modal = document.getElementById('simpleModal');
                 modal.style.display = 'none';
             }
         }
+
+const lastImg = 1;
+document.getElementById(0).src = document.getElementById(lastImg).src;
+document.getElementById(lastImg).className = "thumb selected";
+
+function preview(img) {
+    console.log(preview);
+    document.getElementById(lastImg).className = "thumb normal";
+    img.className = "thumb selected";
+    document.getElementById(0).src = img.src;
+    lastImg = img.id
+}

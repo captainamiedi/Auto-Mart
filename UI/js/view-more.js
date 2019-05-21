e519c4a281321da8debd40ef75eef47f9f5b4b47
iambright <captainamiedi1@gmail.com>
2019-05-21 05:40:05 +0100

    modal added

--- UI/js/view-more.js
+++ UI/js/view-more.js
@@ -1,23 +1,25 @@
 const modal = document.getElementById('simpleModal');
-        const modalBtn = document.getElementById('modalBtn');
-        const closeBtn = document.getElementsByClassName('closeBtn')[0];
-        modalBtn.addEventListener('click', openModal);
+const modalBtn = document.getElementById('modalBtn');
+const modalBtn1 = document.getElementById('modalBtn1');
+const closeBtn = document.getElementsByClassName('closeBtn')[0];
+modalBtn.addEventListener('click', openModal);
+modalBtn1.addEventListener('click', openModal);
+window.addEventListener('click', outsideClick);
 
-        window.addEventListener('click', outsideClick);
+closeBtn.addEventListener('click', closeModal);
 
-        closeBtn.addEventListener('click', closeModal);
-        function openModal(){
-            console.log('1133')
-            modal.style.display = 'block';
-        }
-        function closeModal(){
-            modal.style.display = 'none';
-        }
-        function outsideClick(e){
-           if(e.target == modal){
-                modal.style.display = 'none';
-            }
-        }
+function openModal(){
+    console.log('1133')
+    modal.style.display = 'block';
+}
+function closeModal(){
+    modal.style.display = 'none';
+}
+function outsideClick(e){
+    if(e.target == modal){
+        modal.style.display = 'none';
+    }
+}
  
 function changeImage(event){
     const targetElement = event.target

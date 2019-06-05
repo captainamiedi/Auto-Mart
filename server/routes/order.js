8519b9d51cc9c2656eacb26bf9c5f3fb4b4ab52f
captainamiedi <captainamiedi1@.com>
2019-06-05 03:49:52 +0100

    feature(endpoint):an endpoint to delete a car_id
    [finishes #166213577]

--- server/routes/order.js
+++ server/routes/order.js
@@ -5,7 +5,11 @@ import {
 } from '../controller/dbController/order';
 // eslint-disable-next-line import/named
 import { signup, login } from '../controller/dbController/user';
-import {specific_car, available_cars, price_range_cars} from '../controller/dbController/cars';
+import {
+  specific_car, available_cars, price_range_cars,
+  remove_cars,
+} from '../controller/dbController/cars';
+import db from '../model/dummy_db/cars';
 const router = express.Router();
 
 router.post('/api/v1/order', purchase);
@@ -18,5 +22,7 @@ router.post('/api/v1/login', login);
 router.get('/api/v1/car/:id', specific_car);
 //router.get('/api/v1/car', available_cars);
 router.get('/api/v1/car', price_range_cars);
+router.delete('/api/v1/car/:id', remove_cars);
+
 
 export default router;

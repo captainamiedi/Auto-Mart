ddcc2725d286e5b1b29997beb337b296626f9480
captainamiedi <captainamiedi1@.com>
2019-06-17 20:44:28 +0100

    feature(endpoint):user should be able login with password and email
    [finishes #166710451]

--- server/test/db_Test/user.js
+++ server/test/db_Test/user.js
@@ -7,24 +7,26 @@ import app from '../../index';
 chai.use(chaiHttp);
 chai.should();
 
+//console.log(process.env.NODE_ENV, 'testing.........');
+
 const user = {
   id: '888ac18b-a04b-40b7-b57e-e550496e8323',
   first_name: 'math',
   last_name: 'peter',
-  email: 'test13@gmail',
+  email: 'test13@gmail.com',
   password: '12345678aB@',
   address: '10 james street',
   is_admin: true,
 };
 
-describe('API for AutoMart', ()=> {
-  before(()=>{
+// describe('API for AutoMart', ()=> {
+//   before(()=>{
 
-  });
-  after(()=>{
+//   });
+//   after(()=>{
 
-  });
-});
+//   });
+// });
 
 describe('USERS SIGNUP', () => {
   it('should allow user signup', (done)=> {
@@ -33,7 +35,6 @@ describe('USERS SIGNUP', () => {
       .send(user)
       .end((err, res)=> {
         //console.log(res.body, 'response.body');
-
         res.should.have.status(201);
         res.body.should.be.a('object');
         done();
@@ -66,6 +67,42 @@ describe('USERS SIGNUP', () => {
       .end((err, res)=> {
         res.should.have.status(400);
         res.body.should.be.a('object');
+        done();
+      });
+  });
+});
+
+describe('LOGIN USER', ()=>{
+  it('should allow user to login', (done)=>{
+    chai.request(app)
+      .post('/api/v1/login')
+      .send(user)
+      .end((err, res)=>{
+        res.should.have.status(200);
+        done();
+      });
+  });
+  it('should not allow a user to login without password', (done) => {
+    chai.request(app)
+      .post('/api/v1/login')
+      .send({
+        email: 'test11@gmail.com',
+        password: '',
+      })
+      .end((err, res)=>{
+        res.should.have.status(400);
+        done();
+      });
+  });
+  it('should not allow a user to login without email', (done) => {
+    chai.request(app)
+      .post('/api/v1/login')
+      .send({
+        email: '',
+        password: '12345678aB@',
+      })
+      .end((err, res)=>{
+        res.should.have.status(400);
         done();
       });
   });

821eb2b49b1021a1f767641e7b923bf3c193d7e8
captainamiedi <captainamiedi1@gmail.com>
2019-07-14 18:53:23 +0100

    fixing bug for autogradr

--- server/routes/order.js
+++ server/routes/order.js
@@ -10,30 +10,30 @@ import {
 } from '../controller/dbController/car';
 import authCheck from '../utils/auth_checker';
 import { purchase, update_price, userHistoryOrder } from '../controller/dbController/order';
-import {
-  signupValidator,
-  loginValidator,
-  carSaleValidator,
-  updateValidator,
-} from '../utils/validate';
+// import {
+//   signupValidator,
+//   loginValidator,
+//   carSaleValidator,
+//   updateValidator,
+// } from '../utils/validate';
 import flag from '../controller/dbController/flag';
 import { multerUploads } from '../utils/imageUpload';
 
 
 const router = express.Router();
 
-router.post('/api/v1/order', authCheck, purchase);
-router.post('/api/v1/car', multerUploads, authCheck, carSaleValidator, car_sale);
-router.patch('/api/v1/order/:order-id/price', update_price);
-router.patch('/api/v1/car/:id/status', authCheck, mark_sold);
-router.patch('/api/v1/car/:id/price', authCheck, update_car_price);
+router.post('/order', authCheck, purchase);
+router.post('/car', multerUploads, authCheck, car_sale);
+router.patch('/order/:order-id/price', update_price);
+router.patch('/car/:id/status', authCheck, mark_sold);
+router.patch('/car/:id/price', authCheck, update_car_price);
 router.post('/auth/signup', signup);
 router.post('/auth/signin', login);
-router.get('/api/v1/car/:id', authCheck, specific_car);
-router.get('/api/v1/car', authCheck, view_status_price);
-router.get('/api/v1/user', authCheck, userHistory);
-router.get('/api/v1/order/user', authCheck, userHistoryOrder);
-router.post('/api/v1/flag', authCheck, flag);
-router.delete('/api/v1/car/:id', authCheck, delete_car);
+router.get('/car/:id', authCheck, specific_car);
+router.get('/car', authCheck, view_status_price);
+router.get('/user', authCheck, userHistory);
+router.get('/order/user', authCheck, userHistoryOrder);
+router.post('/flag', authCheck, flag);
+router.delete('/car/:id', authCheck, delete_car);
 
 export default router;

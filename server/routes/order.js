a179e8390235bc23d3968d72818e54c390acb37c
captainamiedi <captainamiedi1@gmail.com>
2019-07-16 00:15:50 +0100

    fixing bug

--- server/routes/order.js
+++ server/routes/order.js
@@ -6,7 +6,7 @@ import express from 'express';
 //import { signup, login } from '../controller/dummy_dbController/user';
 import { signup, login } from '../controller/dbController/user';
 import {
-  car_sale, mark_sold, specific_car, userHistory, view_status_price, delete_car,
+  car_sale, mark_sold, specific_car, userHistory, delete_car,
 } from '../controller/dbController/car';
 import authCheck from '../utils/auth_checker';
 import { purchase, update_price, userHistoryOrder } from '../controller/dbController/order';
@@ -22,18 +22,18 @@ import { multerUploads } from '../utils/imageUpload';
 
 const router = express.Router();
 
-router.post('/order', authCheck, purchase);
-router.post('/car', multerUploads, authCheck, car_sale);
-router.patch('/order/:order-id/price', update_price);
-router.patch('/car/:id/status', authCheck, mark_sold);
+// router.post('/order', authCheck, purchase);
+// router.post('/car', multerUploads, authCheck, car_sale);
+// router.patch('/order/:order-id/price', update_price);
+// router.patch('/car/:id/status', authCheck, mark_sold);
 // router.patch('/car/:id/price', authCheck, update_car_price);
 router.post('/auth/signup', signup);
 router.post('/auth/signin', login);
-router.get('/car/:id', authCheck, specific_car);
+// router.get('/car/:id', authCheck, specific_car);
 // router.get('/car', authCheck, view_status_price);
-router.get('/user', authCheck, userHistory);
-router.get('/order/user', authCheck, userHistoryOrder);
-router.post('/flag', authCheck, flag);
-router.delete('/car/:id', authCheck, delete_car);
+// router.get('/user', authCheck, userHistory);
+// router.get('/order/user', authCheck, userHistoryOrder);
+// router.post('/flag', authCheck, flag);
+// router.delete('/car/:id', authCheck, delete_car);
 
 export default router;

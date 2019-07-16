6d47b3c2e065d7fa6c564856532bfca6a5df241a
captainamiedi <captainamiedi1@gmail.com>
2019-07-16 01:37:26 +0100

    fixing bug

--- server/controller/dbController/car.js
+++ server/controller/dbController/car.js
@@ -7,8 +7,8 @@ import { carResponseMsg } from '../../utils/helpers';
 export const car_sale = async (req, res) => {
   //const selectUser = 'SELECT id FROM users WHERE email = $1';
   // const email = [req.body.email];
-  const carSaleQuery = 'INSERT INTO cars (id, state, price, status, manufacturer, model, body_type, created_date, owner_id) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) returning *';
-  const carSaleQueryImg = 'INSERT INTO cars (id, state, image, price, status, manufacturer, model, body_type, created_date, owner_id) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) returning *';
+  const carSaleQuery = 'INSERT INTO cars (id, state, price, status, manufacturer, model, body_type, created_date) VALUES($1, $2, $3, $4, $5, $6, $7, $8) returning *';
+  const carSaleQueryImg = 'INSERT INTO cars (id, state, image, price, status, manufacturer, model, body_type, created_date) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) returning *';
 
 
   try {
@@ -27,7 +27,7 @@ export const car_sale = async (req, res) => {
         req.body.model,
         req.body.body_type,
         new Date(),
-        req.authData.id,
+        // req.authData.id,
       ];
   
       const result = await db.query(carSaleQuery, values);
@@ -48,7 +48,7 @@ export const car_sale = async (req, res) => {
         req.body.model,
         req.body.body_type,
         new Date(),
-        req.authData.id,
+        // req.authData.id,
       ];
   
       const result = await db.query(carSaleQueryImg, valuesImg);

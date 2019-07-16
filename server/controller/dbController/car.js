9c656cbe7303b9c0d21d22d01d54a1e17b1087ee
captainamiedi <captainamiedi1@gmail.com>
2019-07-16 15:08:22 +0100

    fixing autogradr bug

--- server/controller/dbController/car.js
+++ server/controller/dbController/car.js
@@ -7,7 +7,7 @@ import { carResponseMsg } from '../../utils/helpers';
 export const car_sale = async (req, res) => {
   //const selectUser = 'SELECT id FROM users WHERE email = $1';
   // const email = [req.body.email];
-  const carSaleQuery = 'INSERT INTO cars (state, image, price, status, manufacturer, model, body_type, created_date, owner_id) VALUES($1, $2, $3, $4, $5, $6, $7, $8) returning *';
+  const carSaleQuery = 'INSERT INTO cars (state, image, price, status, manufacturer, model, body_type, created_date, owner_id) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) returning *';
   const carSaleQueryImg = 'INSERT INTO cars (state, image, price, status, manufacturer, model, body_type, created_date, owner_id) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) returning *';
 
 
@@ -25,10 +25,10 @@ export const car_sale = async (req, res) => {
     // console.log(image, 'image....');
 
     const values = [
-      // uuidv4(),
       req.body.state,
       req.body.img_url,
       req.body.price,
+      req.body.status,
       req.body.manufacturer,
       req.body.model,
       req.body.body_type,
